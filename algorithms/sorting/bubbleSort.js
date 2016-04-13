fabbacbe72dca4e1a65f0dff89d08e31e4af0d0c
Roland Pangu <faruzzy@gmail.com>
2016-04-12 19:12:21 -0700

    Added optimization to allow the best case to be O(n)

--- algorithms/sorting/bubbleSort.js
+++ algorithms/sorting/bubbleSort.js
@@ -1,14 +1,27 @@
 var bubbleSort = function(array) {
+	if (arguments.length === 0 || Array.isArray(array)) {
+		throw new Error();
+	}
+
 	function swap(array, i, j) {
 		var temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
 
-	for (var i = 0, len = array.length; i < len; i++)
-		for (var j = 0; j < len; j++)
-			if (array[j] > array[j + 1])	
+	for (var i = 0, len = array.length; i < len; i++) {
+		var changed = false;
+		for (var j = 0; j < len; j++) {
+			if (array[j] > array[j + 1]) {
+				hasChanged = true;
 				swap(array, j, j + 1);
+			}
+		}
+
+		if (!hasChanged) {
+			return array;
+		}
+	}
 	return array;
 };
 
