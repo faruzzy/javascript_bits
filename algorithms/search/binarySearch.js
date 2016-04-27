3148a1d1cd20184c7e4f2ac8105b302f35735236
Roland Pangu <faruzzy@gmail.com>
2016-04-26 18:37:24 -0700

    BinarySearch works on sorted array!

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -22,4 +22,6 @@ var binarySearch = function(array, value) {
 	return -1;
 };
 
-var array = [10, 2, 4, 7, 8, 5, 1, 3, 6];
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(binarySearch(array, 20));
+console.log(binarySearch(array, 3));
