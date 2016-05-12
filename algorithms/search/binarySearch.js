fdf8a0b834a679afbdc2e5d1b965494e4b49f2b7
Roland Pangu <faruzzy@gmail.com>
2016-05-12 09:24:51 -0700

    Fix edge case bug

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -2,13 +2,16 @@ var binarySearch = function(array, value) {
 	if (array.constructor !== Array)
 		throw new Error('Expecting an array as the argrument');
 
+	if (!Number.isInteger(value))
+		throw new Error('Expecting an integer for the second argument');
+
 	if (array.length === 0) 
 		return -1;
 
 		var low = 0, 
 			high = array.length - 1;
 
-	while (low < high) {
+	while (low <= high) {
 		var mid = Math.floor((low + high) / 2);
 		if (array[mid] === value) {
 			return mid;
@@ -25,3 +28,4 @@ var binarySearch = function(array, value) {
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(binarySearch(array, 20));
 console.log(binarySearch(array, 3));
+console.log(binarySearch(array, 4));
