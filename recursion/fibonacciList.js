7bb1a352191ea180f5dae6aa09e10f0b9dfd6a63
Roland Pangu <faruzzy@gmail.com>
2016-05-13 16:38:26 -0700

    Fix small bug in first solution and added a cleaner solution

--- recursion/fibonacciList.js
+++ recursion/fibonacciList.js
@@ -2,7 +2,7 @@
 function fibonacciList(count, n, prev, next, list) {
 	if (count < n) {
 		if (count === 0) {
-			list.push(0);
+			list.push(1);
 			fibonacciList(++count, n, prev, next, list);
 		} else if (count === 1) {
 			list.push(1);
@@ -17,4 +17,18 @@ function fibonacciList(count, n, prev, next, list) {
 	}
 	return list.join();
 }
-console.log(fibonacciList(0, 10, 0, 1, []));
+
+// cleaner
+function fibonacciList2(n) {
+	function fibonacci(n) {
+		if (n === 1 || n === 2) return 1;
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	var list = [];
+	for (var i = 1; i <= n; i++)
+		list.push(fibonacci(i));
+	return list.join();
+}
+
+console.log(fibonacciList(0, 10, 1, 1, []));
+console.log(fibonacciList2(10));
